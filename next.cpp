3202abe8c1f6a2b7af0cb4ffe04404d229e6a5c1
kaitoyamazaki <reflect018@yahoo.co.jp>
2021-05-06 16:58:22 +0900

    GWs task

---
+++ next.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	string a,b;
+	int maru[1000];
+	int fg = 0;
+
+	cin >> a >> b;
+
+	int n1,n2,n;
+	n1 = a.size();
+	n2 = b.size();
+	if(n1 > n2)
+		n = n1;
+	else
+		n = n2;
+
+	if(n1 > n2)
+	{
+		for(int i = 0; i < n;i++)
+		{
+			if(a[i] == b[i])
+			{
+				maru[fg] = 1;
+				fg++;
+			}
+		
+			else if(a[i] != b[i])
+			{
+				maru[fg] = 0;
+				fg++;
+				b.insert(i," ");
+			}
+		}
+		cout << a << endl;
+		cout << b << endl;
+	}	
+	
+	else if(n1 < n2)
+	{
+		for(int i = 0; i < n;i++)
+		{
+			if(b[i] == a[i])
+			{
+				maru[fg] = 1;
+				fg++;
+			}
+		
+			else if(b[i] != a[i])
+			{
+				maru[fg] = 0;
+				fg++;
+				a.insert(i," ");
+			}
+		}
+
+		cout << a << endl;
+		cout << b << endl;
+	}
+
+	for(int i = 0; i < n; i++)
+	{
+		if(maru[i] == 1)
+			cout << "○";
+		else
+			cout << "✗";
+	}
+
+	cout << endl;
+}
+
